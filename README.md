1a1814e6c197cdcd6b3028b40bc00d88eef531d0
DEEP MEHTA <deep.mm@somaiya.edu>
2023-02-12 00:46:51 +0000

    Update shell scripts and add workflows

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Analysis-Sorting-Algorithms
 This repository contains programs to analyze and compare various sorting algorithms
+
+To create an input file with random numbers:
+```
+python random_permutation.py 10000 -b 5 > random.txt
+```
+
+To use sorting algorithm:
+```
+./run_sort.sh {name_of_algo, e.g. insertion} < random.txt
+```
--- run_sort.sh
+++ run_sort.sh
@@ -1,5 +1,17 @@
+printf "Hardware Details:\n"
+printf "\nOperating System, Type and Version\n"
+cat /proc/version
+printf "\nCPU Info\n"
+grep -E 'processor|vendor_id|model name|processor|cpu MHz|cache size' /proc/cpuinfo
+printf "\nMemory Info\n"
+grep -E 'MemTotal' /proc/meminfo | numfmt --field 2 --from-unit=Ki --to=iec | sed 's/ kB//g'
+
 if [ $1 = insertion ]; then
-    # run insertion sort
+    java Sort "insertion"
+    while read -r line; do
+      echo $line
+    done
+    echo "."
 elif [ $1 = merge ]; then
     # run merge sort
 elif [ $1 = quick ]; then
