188a042fefb3ae6514379a59d6df25e88490dacc
Deep Mehta <deep.mm@somaiya.edu>
2023-02-22 00:53:44 -0500

    Pushing csv results

--- run_experiments_insertion.sh
+++ run_experiments_insertion.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 echo "n,comparison,runtime" > insertion.csv
 
 # Array starting from 100000 to 10000000 with step 100000
-for n in {1000..100000..1000}
+for ((n=1000;n<=100000;n+=1000))
 do
-    python random_permutation.py $n -b $n > random_$n.txt
+    python3 random_permutation.py $n -b $n > random_$n.txt
     # Append n to the csv row before the sort output
     ./run_sort.sh insertion < <(tac random_$n.txt) >> insertion.csv
 done
